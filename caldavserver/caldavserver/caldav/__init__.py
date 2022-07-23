c1fad1a72f1a104e44c641f7d29db467f501c788
DanHugoDanHugo <dhugo@reallycool.com>
2022-07-23 15:34:50 -0700

    feat: add public handlers to __all__ in caldav/__init__.py

--- caldavserver/caldavserver/caldav/__init__.py
+++ caldavserver/caldavserver/caldav/__init__.py
@@ -15,3 +15,5 @@
 # You should have received a copy of the GNU General Public License
 # along with be-smart-calendar-server-py.  If not, see <http://www.gnu.org/licenses/>.
 
+
+__all__ = ['audio', 'delete', 'get', 'head', 'mkcalendar', 'mkcol', 'options', 'post', 'propfind', 'proppatch', 'put', 'report']
