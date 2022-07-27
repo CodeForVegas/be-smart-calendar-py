a602aaa672981cb71a817cbac4105a4b6a57d254
DanHugoDanHugo <dhugo@reallycool.com>
2022-07-27 05:57:39 -0700

    feat: first stub of calendar object in app with initial test

---
+++ caldavserver/tests/app/test_calendar.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2022 Code for Vegas Foundation
+# 
+# This file is part of be-smart-calendar-server-py.
+# 
+# be-smart-calendar-server-py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# be-smart-calendar-server-py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with be-smart-calendar-server-py.  If not, see <http://www.gnu.org/licenses/>.
+
+import vobject
+
+from caldavserver.app.calendar import Calendar
+
+
+def test_instantiateCalendar():
+	calendar_instance = Calendar()
+	assert calendar_instance is not None
+
+def test_parseValidVCalendar():
+	calendar_instance = Calendar()
+
