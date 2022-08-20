59559bb69d30de15989ea5a8320dc7d701f3aea7
DanHugoDanHugo <dhugo@reallycool.com>
2022-08-20 07:39:32 -0700

    feat: initial commit of new app component stubs

---
+++ caldavserver/tests/app/test_availability.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2022 Code for Vegas Foundation
+# 
+# This file is part of be-smart-calendar-server-py.
+# 
+# be-smart-calendar-server-py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# be-smart-calendar-server-py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with be-smart-calendar-server-py.  If not, see <http://www.gnu.org/licenses/>.
+
+from caldavserver.app.availability import AvailabilityForeignData, AvailabilityData, Availability
+
+
+def test_instantiateAvailabilityForeignData():
+	availabilityforeigndata_instance = AvailabilityForeignData()
+	assert availabilityforeigndata_instance is not None
+
+def test_instantiateAvailabilityData():
+	availabilitydata_instance = AvailabilityData()
+	assert availabilitydata_instance is not None
+
+def test_instantiateAvailability():
+	availability_instance = Availability()
+	assert availability_instance is not None
+
